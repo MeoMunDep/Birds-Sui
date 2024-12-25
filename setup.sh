f7cdbd7345b02c79fdb85bd52fb40640131db78f
BlackCat <184516521+MeoMunDep@users.noreply.github.com>
2024-12-26 04:26:55 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -30,16 +30,13 @@ fi
 create_default_configs() {
     cat > configs.json << EOL
 {
-    "timeZone": "en-US",
-    "rotateProxy": false,
-    "skipInvalidProxy": false,
-    "proxyRotationInterval": 2,
-    "delayEachAccount": [5, 8],
-    "timeToRestartAllAccounts": 300,
-    "howManyAccountsRunInOneTime": 10,
-    "doTasks": true,
-    "playGames": true,
-    "referralCode": ""
+  "limit": 100,
+  "countdown": 300,
+  "country_time": "en-US",
+  "boost": true,
+  "howManyBirdsToContribute": 1000,
+  "delayEachAccount": [5, 8],
+  "referralCode":""
 }
 EOL
 }
@@ -55,7 +52,7 @@ check_configs() {
 while true; do
     clear
     echo "============================================================================"
-    echo "    name BOT SETUP AND RUN SCRIPT"
+    echo "    Birds Sui BOT SETUP AND RUN SCRIPT"
     echo "============================================================================"
     echo
     echo "Current directory: $(pwd)"
