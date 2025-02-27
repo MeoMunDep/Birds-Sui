acd0db25b9e1c3c6effb4b143bf3e9162997cb2d
BlackCat <184516521+MeoMunDep@users.noreply.github.com>
2025-02-27 13:12:33 +0700

    ....

--- Birds/setup.bat
+++ Birds/setup.bat
@@ -1,5 +1,5 @@
 @echo off
-title Bird Sui Bot
+title Bird Sui Bot by @MeoMunDep
 color 0A
 
 cd ..
@@ -14,7 +14,7 @@ if exist node_modules (
 :MENU
 cls
 echo =================================================================
-echo    Bird Sui BOT SETUP AND RUN SCRIPT
+echo    Bird Sui BOT SETUP AND RUN SCRIPT by @MeoMunDep
 echo =================================================================
 echo.
 echo Current directory: %CD%
@@ -94,7 +94,7 @@ if exist "..\node_modules" (
 ) else (
     echo Using node_modules from current directory
 )
-cd birds && node bot
+node MeoMunDep
 pause
 goto MENU
 
